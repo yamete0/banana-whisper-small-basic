08c118f724dc5be360c19c9539a7aa45ca34eca2
yamete <102901194+yamete0@users.noreply.github.com>
2023-04-25 00:02:26 +0300

    Update download.py

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ import whisper
 import torch
 
 def download_model():
-    model = whisper.load_model("base")
+    model = whisper.load_model("small.en")
 
 if __name__ == "__main__":
     download_model()
